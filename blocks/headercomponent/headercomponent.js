463e51fde392698c8c2664e1544fa183301d9599
TALARI PRATYUSHA <talari.pratyusha@capgemini.com>
2025-05-22 18:10:18 +0530

    Update headercomponent.js

--- blocks/headercomponent/headercomponent.js
+++ blocks/headercomponent/headercomponent.js
@@ -5,7 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
     navLabel.addEventListener("click", () => {
       const parent = navLabel.parentElement;
       const items = parent.querySelectorAll('[data-aue-prop="nav1_item1_label"]');
+       const items = parent.querySelectorAll('[data-aue-prop="nav1_item1_icon"]');
        const items = parent.querySelectorAll('[data-aue-prop="nav1_item2_label"]');
+       const items = parent.querySelectorAll('[data-aue-prop="nav1_item2_icon"]');
 
       items.forEach(item => {
         item.classList.toggle("show");
