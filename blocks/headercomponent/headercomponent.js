9a25d3dfb02ff2b50b3b5a0e89d69c877c9fdaa6
TALARI PRATYUSHA <talari.pratyusha@capgemini.com>
2025-05-23 17:29:29 +0530

    Update headercomponent.js

--- blocks/headercomponent/headercomponent.js
+++ blocks/headercomponent/headercomponent.js
@@ -1,16 +1,1 @@
-<script>
-document.addEventListener("DOMContentLoaded", function () {
-  const searchLabel = document.querySelector('[data-aue-prop="search_placeholder"]');
-  const searchIcon = document.querySelector('[data-aue-prop="search_icon"]');
 
-  if (searchLabel && searchIcon) {
-    const iconParent = searchIcon.closest("p");
-    const labelParent = searchLabel.closest("p");
-
-    // Move the icon before the label
-    if (iconParent && labelParent && labelParent.parentNode) {
-      labelParent.parentNode.insertBefore(iconParent, labelParent);
-    }
-  }
-});
-</script>
