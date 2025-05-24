4ac40b89e0721aa5d50fe559245e34f91d93f303
TALARI PRATYUSHA <talari.pratyusha@capgemini.com>
2025-05-24 23:21:36 +0530

    Update headercomponent.js

--- blocks/headercomponent/headercomponent.js
+++ blocks/headercomponent/headercomponent.js
@@ -1,14 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.body.addEventListener("click", function (event) {
-    const label = event.target.closest('[data-aue-prop$="_label"]');
-    if (!label) return;
 
-    const labelProp = label.getAttribute("data-aue-prop");
-    const navPrefix = labelProp.split("_")[0];
-
-    const navItems = document.querySelectorAll(`[data-aue-prop^="${navPrefix}_item"]`);
-    navItems.forEach(item => {
-      item.classList.toggle("active");
-    });
-  });
-});
