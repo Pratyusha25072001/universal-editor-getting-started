a137c198bb78d0e5771b9433c94fa78f289183f4
TALARI PRATYUSHA <talari.pratyusha@capgemini.com>
2025-05-25 00:54:15 +0530

    Update headercomponent.js

--- blocks/headercomponent/headercomponent.js
+++ blocks/headercomponent/headercomponent.js
@@ -7,13 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
       const navPrefix = labelProp.split("_")[0]; // e.g., "nav1", "nav2", etc.
 
       const navItems = document.querySelectorAll(`[data-aue-prop^="${navPrefix}_item"]`);
-
       navItems.forEach(item => {
-        const button = item.querySelector("a.button");
-        const icon = document.querySelector(`[data-aue-prop="${item.getAttribute("data-aue-prop").replace("_label", "_icon")}"]`);
-
-        if (button) button.classList.toggle("active");
-        if (icon) icon.classList.toggle("active");
+        item.classList.toggle("active");
       });
     });
   });
